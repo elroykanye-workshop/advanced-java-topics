e7dd0f004fb8d709c99ae6b5ba7462028c379b75
Elroy Kanye <elroykanye@gmail.com>
2022-04-06 22:48:47 +0100

    complete ajt11

---
+++ ajt11-javaio/src/elroykanye/JavaIO.java
@@ -0,0 +1,9 @@
+package elroykanye;
+
+public class JavaIO {
+    public static final String REPO = "ajt11-javaio/src/elroykanye/";
+
+    public static void main(String[] args) {
+        KFile.runner();
+    }
+}
